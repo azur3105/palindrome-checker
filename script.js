090364320aa21ef5846d2243bbd3a78df3afe3a5
azur3105 <145514696+azur3105@users.noreply.github.com>
2024-04-09 08:42:50 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,34 @@
+// Getting elements from DOM
+const checkBtn = document.getElementById("check-btn");
+const result = document.getElementById("result");
+const textInput = document.getElementById("text-input")
+
+const buttonClick = function(){
+const textInput = document.getElementById("text-input").value.toLowerCase();
+  const cleanedText = textInput.replace(/[^a-z0-9]/g, '');
+  const reversedText = cleanedText.split('').reverse().join('');
+
+// Checking if the input is as reversed (palindrom) ignoring punctuation, case, and spacing
+  if(cleanedText === ''){
+    alert("Please input a value");
+
+  } else if(cleanedText === reversedText){
+    result.style="display:block"
+    result.innerHTML = `<b>${textInput}</b> is a palindrome`;
+  
+  } else {
+    result.innerHTML = `<b>${textInput}</b> is not a palindrome`;
+    result.style="display:block; border-color:#ffb0b0; background-color: #fff7f7"
+  }
+  }
+
+// Listen to Click on a Check button
+checkBtn.addEventListener("click", () => {
+  buttonClick();  
+});
+
+// Listen to Enter click on input field
+textInput.addEventListener("keypress", (e) => {
+if(e.key === 'Enter')
+  buttonClick();  
+});
